4b4c3a174bae1fb8a0c60c904fcf98ebc253b94e
Sabin Adams <sabintheworld@gmail.com>
2022-03-24 16:42:14 -0700

    Filtering works

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,19 +1,26 @@
 import { UserWithProfile } from "~/util/interfaces"
 import { SearchBox } from "./SearchBox"
 import { SelectBox } from "./SelectBox"
-import { useNavigate } from "remix"
+import { useNavigate, useSearchParams } from "remix"
 import { UserCircle } from "./UserCircle"
 
 export function TopBar({ user }: { user: UserWithProfile }) {
     const navigate = useNavigate()
+    let [searchParams] = useSearchParams();
+
+    const clearFilters = () => {
+        searchParams.delete('filter')
+        searchParams.delete('sort')
+        navigate('/home')
+    }
 
     return (
         <form className="w-full px-6 flex items-center gap-x-4 border-b-4 border-b-blue-900 border-opacity-30 h-20">
-            <SearchBox className="w-full rounded-xl px-3 py-2" containerClassName='w-2/5' />
+            <SearchBox className="w-full rounded-xl px-3 py-2" containerClassName='w-2/5' name="filter" />
             <SelectBox
-                value={'date'}
                 className="w-full rounded-xl px-3 py-2 text-gray-400"
                 containerClassName='w-40'
+                name="sort"
                 options={[{
                     name: 'Date',
                     value: 'date'
@@ -28,6 +35,11 @@ export function TopBar({ user }: { user: UserWithProfile }) {
             <button type="submit" className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
                 Search
             </button>
+            {searchParams.get('filter') &&
+                <button onClick={clearFilters} className="rounded-xl bg-red-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
+                    Clear Filters
+                </button>
+            }
             <div className="flex-1" />
             <UserCircle
                 profile={user.profile}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -18,6 +18,54 @@ export const loader: LoaderFunction = async ({ request }) => {
     // Handles redirect if not authenticated
     const userId = await requireUserId(request)
     const user = await getUser(request)
+    const url = new URL(request.url);
+    const sort = url.searchParams.get("sort");
+    const filter = url.searchParams.get("filter");
+    let sortFilter = {}
+    if (sort) {
+        if (sort === 'date') {
+            sortFilter = {
+                createdAt: 'desc'
+            }
+        }
+        if (sort === 'sender') {
+            sortFilter = {
+                author: {
+                    profile: {
+                        firstName: 'asc'
+                    }
+                }
+            }
+        }
+        if (sort === 'emoji') {
+            sortFilter = {
+                style: {
+                    emoji: 'asc'
+                }
+            }
+        }
+    }
+    let textFilter = {}
+    if (filter) {
+        textFilter = {
+            OR: [
+                {
+                    message: {
+                        mode: 'insensitive',
+                        contains: filter
+                    }
+                },
+                {
+                    author: {
+                        OR: [
+                            { profile: { is: { firstName: { mode: 'insensitive', contains: filter } } } },
+                            { profile: { is: { lastName: { mode: 'insensitive', contains: filter } } } },
+                        ]
+                    }
+                },
+            ]
+        }
+    }
 
     // Select all the users for the users bar (except yourself)
     const users = await prisma.user.findMany({
@@ -41,8 +89,12 @@ export const loader: LoaderFunction = async ({ request }) => {
                 }
             }
         },
+        orderBy: {
+            ...sortFilter
+        },
         where: {
-            recipientId: userId
+            recipientId: userId,
+            ...textFilter
         }
     })
     // Select the most recent three kudos
@@ -76,7 +128,6 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function Index() {
     const { recentKudos, users, user, kudos } = useLoaderData()
-
     return <Layout>
         <Outlet />
         <div className="h-full flex">
@@ -84,7 +135,7 @@ export default function Index() {
             <div className="flex-1 flex flex-col">
                 <TopBar user={user} />
                 <div className="flex-1 flex">
-                    <div className="w-full p-10">
+                    <div className="w-full p-10 flex flex-col gap-y-4">
                         {
                             kudos.map((kudo: KudoWithAuthor) => (
                                 <Kudo key={kudo.id} kudo={kudo} profile={kudo.author.profile} />
